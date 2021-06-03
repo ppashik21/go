8c9cc7274515dde25178747055ce42c8bbc7c959
Pavel <78554808+ppashik21@users.noreply.github.com>
2021-06-03 17:07:48 +0300

    Update go.go

--- go.go
+++ go.go
@@ -1,12 +1,15 @@
 package main
-import ("fmt"
-        "net/http")
 
-func home_home(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello")
-  }
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
 
 func main() {
-  http.HandleFunc("/", home_page)
-  http.ListenAndServe(":80", nil)
-  }
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
+}
